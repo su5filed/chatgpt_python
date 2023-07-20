ce1cb1b39748e556d1e3cc08b8f30553aa594c54
su5filed <137695729+su5filed@users.noreply.github.com>
2023-07-20 11:04:43 +0900

    Add files via upload

--- chatgpt_and_bard_2023.py
+++ chatgpt_and_bard_2023.py
@@ -5,8 +5,19 @@ import PySimpleGUI as sg
 
 from bardapi import Bard
 
+
+import time
+import threading
+import datetime
+import os
+
 api_key = 'sk-.....'
 openai.api_key = api_key
+
+folder = 'log'
+
+logfilename_html = ""
+logfilename_txt = ""
 
 #chat GPTの応答
 def generate_text_chatgpt(prompt, conversation_history):
@@ -63,9 +74,7 @@ def show_dialog():
     # ウィンドウオブジェクトの作成
     window = sg.Window('title', layout, size=(700, 480), resizable=True)
 
-    # 会話履歴を格納するためのリストを初期化
-    conversation_history = []
-    conversation_history_bard = []
+    logh("<html><table>")
 
     # イベントのループ
     while True:
@@ -77,6 +86,7 @@ def show_dialog():
         
         # 変更部分終わり
         if event == 'CLOSE':
+            logh("</table></html>")
             break
 
         if event == 'CLEAR':
@@ -87,33 +97,71 @@ def show_dialog():
             break
         
         if( event == 'SEND' and len(values) > 0 ):
-            input_prompt = values['Multiline1']
-            generated_text = generate_text_chatgpt(input_prompt, conversation_history)
-            window['Multiline2'].update(generated_text)
-            print("応答 chatgpt:", generated_text)
-            generated_text = generate_text_bard(input_prompt, conversation_history_bard)
-            window['Multiline3'].update(generated_text)
-            print("応答 bard:", generated_text)
-        
-        
-        # ウィンドウのサイズ変更イベントの場合
-        if event == '__TIMEOUT__':
-            # ウィンドウのサイズを取得
-            window_width, window_height = window.size
-            print("window_height:", window_height)
             
-            # Multiline1とMultiline2の高さを比率に応じて調整
-            new_height1 = (window_height - initial_height) // 3  # Multiline1の新しい高さ
-            new_height2 = (window_height - initial_height) // 3 * 2  # Multiline2の新しい高さ
+            input_prompt = values['Multiline1']
 
-            # Multiline1とMultiline2の高さを変更
-            window['Multiline1'].Update(size=(window_width, new_height1))
-            window['Multiline2'].Update(size=(window_width, new_height2))
-            window['Multiline3'].Update(size=(window_width, new_height2))
+            # 新しいスレッドを作成し、wait()関数を実行する
+            t = threading.Thread(target=sleep_thread, args=[30,input_prompt,window])
+            t.start()
+            # スレッドがsleepしている間、他の処理を実行する
+            print("スレッドがsleepしています")
+            # スレッドがsleep状態から解除されるまで待つ
+            # t.join()
+            print("スレッドがsleep状態から解除されました")
 
+        
     # ウィンドウ終了処理
     window.close()
 
 
+# 新しいスレッドで実行する関数
+def sleep_thread(seconds,input_prompt,window):
+    # 会話履歴を格納するためのリストを初期化
+    # time.sleep(seconds)
+    conversation_history = []
+    conversation_history_bard = []
+    # 押せないようにする
+    window['SEND'].update(disabled=True)
+    logw("HUMAN", input_prompt)
+    generated_text = generate_text_chatgpt(input_prompt, conversation_history)
+    window['Multiline2'].update(generated_text)
+    print("応答 chatgpt:", generated_text)
+    logw("CHATGPT", generated_text)
+    generated_text = generate_text_bard(input_prompt, conversation_history_bard)
+    window['Multiline3'].update(generated_text)
+    print("応答 bard:", generated_text)
+    logw("BARD", generated_text)
+    # 押せるようにする
+    window['SEND'].update(disabled=False)
+
+
+def logh(line):
+    # ログファイルを開きます
+    with open(logfilename_html, 'a') as file:
+        # ログを出力します
+        file.write(line+'\n')
+    # ログファイルをクローズします
+    file.close()
+
+def logw(TAG, line):
+    color="black"
+    if TAG=="BARD":
+        color="blue"
+    elif TAG=="CHATGPT":
+        color="red"
+    elif TAG=="HUMAN":
+        color="green"
+    logh("<tr><td bgcolor="+color+">"+TAG+"</td><td><font color="+color+"><pre>"+line+"</pre></font></td>")        
+    
+
+
 if __name__ == "__main__":
+    
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+    
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    logfilename_html = "log/log_"+timestamp+".html"
+
     show_dialog()
