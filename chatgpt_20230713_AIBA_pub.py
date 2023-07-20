57af816a82acdad5db2725703b3cfa5fa95ef97b
su5filed <137695729+su5filed@users.noreply.github.com>
2023-07-20 13:12:33 +0900

    Add files via upload

--- chatgpt_20230713_AIBA_pub.py
+++ chatgpt_20230713_AIBA_pub.py
@@ -6,16 +6,19 @@ import PySimpleGUI as sg
 import time
 import threading
 import datetime
+import os
 from bardapi import Bard
 
 api_key = 'sk-...'
 openai.api_key = api_key
+folder = 'log'
 logfilename_html = ""
 logfilename_txt = ""
+threading_pub = None
 
 # 新しいスレッドで実行する関数
-def sleep_thread(seconds):
-    time.sleep(seconds)
+def sleep_thread(seconds,input_prompt, window, checkbox1_value, loop_count, InputText_template):
+    loop_chat(input_prompt, window, checkbox1_value, loop_count, InputText_template)
 
 #chat GPTの応答
 def generate_text_chatgpt(count, prompt, conversation_history):
@@ -69,7 +72,9 @@ def show_dialog():
 
     # ウィンドウのレイアウト
     layout = [
-            [sg.Button('SEND', size=(10, 1), key='SEND'),sg.Button('CLEAR', size=(10, 1), key='CLEAR'),sg.Button('CLOSE', size=(10, 1), key='CLOSE') ],
+            [sg.Button('SEND', size=(10, 1), key='SEND'),sg.Button('STOP', size=(10, 1), key='STOP'),sg.Button('CLEAR', size=(10, 1), key='CLEAR'),sg.Button('CLOSE', size=(10, 1), key='CLOSE'),sg.Checkbox('ChatGptのみの自問自答', key='Checkbox1'),
+             sg.Text('回数：', size=(4, 1), key='Question'),sg.InputText('15', size=(10, 1), key='InputText') ],
+            [sg.InputText('ルール：回答には文章の冒頭にA)を添えて答えて下さい。その後、Q)を添えて相手に質問して下さい。', size=(initial_width, 1), key='InputText_template') ],
             [sg.Multiline(default_text="", size=(initial_width,5),  key='Multiline1') ],
             [sg.Multiline(default_text="", size=(initial_width,10), key='Multiline2') ],
             [sg.Multiline(default_text="", size=(initial_width,10), key='Multiline3') ],
@@ -78,9 +83,7 @@ def show_dialog():
     # ウィンドウオブジェクトの作成
     window = sg.Window('title', layout, size=(700, 480), resizable=True)
 
-    # 会話履歴を格納するためのリストを初期化
-    conversation_history = []
-    conversation_history_bard = []
+    logh("<html><table>", logfilename_html)
 
     # イベントのループ
     while True:
@@ -92,50 +95,71 @@ def show_dialog():
         
         # 変更部分終わり
         if event == 'CLOSE':
+            logh("</table></html>", logfilename_html)
             break
 
         if event == 'CLEAR':
             window['Multiline1'].update("")
 
+        if event == 'STOP':
+            # 一定の時間後にスレッドを中断する
+            if threading_pub != None:
+                threading_pub.interrupted = True
+            # 押せるようにする
+            window['SEND'].update(disabled=False)
+        
         # ウィンドウの×ボタンクリックで終了
         if event == sg.WIN_CLOSED:
             break
 
         if( (event == 'SEND' and len(values) > 0)):
-            input_prompt = values['Multiline1']
-            count = 0
-            logw("HUMAN","--- "+str(count)+" ---")
-            logw("HUMAN","count:("+str(count)+")Human:\n"+input_prompt)
-            logo("Human:"+ input_prompt)
-            generated_text = generate_text_chatgpt(count, input_prompt, conversation_history)
-            window['Multiline2'].update(generated_text)
-            print("応答 chatgpt:", generated_text)
-            logo("応答 chatgpt:"+ generated_text)
+            loop_count = values['InputText']
+            if loop_count.isnumeric() == False:
+                sg.popup('入力は数字ではありません')
+            else:
+                input_prompt = values['Multiline1']
+                count = 0
+                logw("HUMAN","--- "+str(count)+" ---")
+                logw("HUMAN","count:("+str(count)+")Human:\n"+input_prompt)
+                logo("Human:"+ input_prompt)
+                
+                checkbox1_value = values['Checkbox1']
+                InputText_template = values['InputText_template']
 
-            loop_chat(input_prompt, window)
+                # 押せないようにする
+                window['SEND'].update(disabled=True)
+                # 新しいスレッドを作成し、wait()関数を実行する
+                threading_pub = threading.Thread(target=sleep_thread, args=[30,input_prompt, window, checkbox1_value, int(loop_count), InputText_template])
+                threading_pub.interrupted = False
+                threading_pub.start()
+            
 
     # ウィンドウ終了処理
     window.close()
 
 
 
-def loop_chat(input_prompt,window):
+def loop_chat(input_prompt,window, checkbox1_value, loop_count, InputText_template):
     
-    template = "ルール：回答には文章の冒頭にA)を添えて答えて下さい。その後、Q)を添えて相手に質問して下さい。\n"
-    # template = ""
+    # template = "ルール：回答には文章の冒頭にA)を添えて答えて下さい。その後、Q)を添えて相手に質問して下さい。\n"
+    # template = "応答する際に、何か質問を添えて下さい。\n"
+    template = InputText_template
     count=1
-
-    # true:ChatGPT自身に問い合わせ、false ChatGptとBardの会話
-    chatgpt_to_chatgpt=False
     
+    # true:ChatGPT自身に問い合わせ、false ChatGptとBardの会話
+    chatgpt_to_chatgpt = checkbox1_value
+
+    # 回数が０以下の場合
+    if loop_count < 1:
+        loop_count = 1
+
     while True:
+        # スレッドが中断された場合はループを抜ける
+        if threading.current_thread().interrupted:
+            print("stop thread.")
+            break
+        
         logw("NONE", "--- "+str(count)+" ---")
-        # Bard に問い合わせ
-        if(chatgpt_to_chatgpt == False):
-            input_prompt = generate_text_bard(count, template+input_prompt)
-            window['Multiline3'].update(input_prompt)
-            print("応答 bard:", input_prompt)
-            logo("応答 bard:"+ input_prompt)
 
         # ChatGPT に問い合わせ
         conversation_history = []
@@ -145,19 +169,30 @@ def loop_chat(input_prompt,window):
         logo("count:"+ str(count))
         print("応答 chatgpt:", input_prompt)
         logo("応答 chatgpt:"+ input_prompt)
+        time.sleep(10)
 
-        # 新しいスレッドを作成し、wait()関数を実行する
-        t = threading.Thread(target=sleep_thread, args=[13])
-        t.start()
-        # スレッドがsleepしている間、他の処理を実行する
-        print("スレッドがsleepしています")
-
-        # スレッドがsleep状態から解除されるまで待つ
-        t.join()
-        print("スレッドがsleep状態から解除されました")
+        # スレッドが中断された場合はループを抜ける
+        if threading.current_thread().interrupted:
+            print("stop thread.")
+            break
+    
+        # Bard に問い合わせ
+        conversation_history = []
+        if(chatgpt_to_chatgpt == False):
+            input_prompt = generate_text_bard(count, template+input_prompt)
+            window['Multiline3'].update(input_prompt)
+            print("応答 bard:", input_prompt)
+            logo("応答 bard:"+ input_prompt)
+            time.sleep(10)
+        
+        
         count+=1
 
-        if count > 15:break
+        if count > loop_count:
+            print("loop end")
+            # 押せるようにする
+            window['SEND'].update(disabled=False)
+            break
     
 def logo(line):
     # ログファイルを開きます
@@ -168,6 +203,7 @@ def logo(line):
     file.close()
 
 def logh(line,logfilename):
+    line.encode("utf-8")
     # ログファイルを開きます
     with open(logfilename, 'a') as file:
         # ログを出力します
@@ -186,8 +222,11 @@ def logw(TAG, line):
 if __name__ == "__main__":
     
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-    logfilename_html = "log_"+timestamp+".html"
-    logfilename_txt = "log_"+timestamp+".txt"
-    logh("<html><table>", logfilename_html)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    
+    logfilename_html = "log/AI_"+timestamp+".html"
+    logfilename_txt = "log/AI_"+timestamp+".txt"
+
     show_dialog()
-    logh("</table></html>", logfilename_html)
